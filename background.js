8a5c45f4e7fad7b941261bc2f2222a90699a630b
Dung Nguyen <flex@Dungs-MacBook-Pro.local>
2018-04-12 20:24:38 +0700

    init

---
+++ background.js
@@ -0,0 +1,3 @@
+browser.browserAction.onClicked.addListener(tab => {
+  chrome.tabs.create({ url: chrome.extension.getURL('index.html') }, tab => {})
+})
---
+++ js/game.js
@@ -0,0 +1,8 @@
+var manager
+document.addEventListener(
+  'DOMContentLoaded',
+  function() {
+    manager = new Get10.Manager()
+  },
+  false
+)
